2e5221e2f55d684712543d2fc92359000291995a
Juan Ignacio Avendaño Huergo <Juan Ignacio Avendao Huergo>
2017-05-12 13:21:13 +0200

    InicioActivity Really done

--- app/src/main/java/solutions/lhdev/app/app/InicioActivity.java
+++ app/src/main/java/solutions/lhdev/app/app/InicioActivity.java
@@ -5,10 +5,15 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
 import android.view.Menu;
 import android.view.View;
+import android.widget.TextView;
 
 public class InicioActivity extends AppCompatActivity {
+
+    private TextView tVPerfil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +21,11 @@ public class InicioActivity extends AppCompatActivity {
         setContentView(R.layout.activity_inicio);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        tVPerfil = (TextView) findViewById(R.id.tVPerfil);
+        SpannableString content = new SpannableString("USER");
+        content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
+        tVPerfil.setText(content);
     }
 
     @Override
